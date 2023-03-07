988f6c6a8337df684631fa4a877fed45eba0a689
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-03-07 15:50:08 +0530

    commit

--- Lets get started with DOM/main.js
+++ Lets get started with DOM/main.js
@@ -18,13 +18,41 @@ item.style.color = 'green';
 
 //      Using getElementTagName
 
-let it = document.getElementsByTagName("li")
-// console.log(it[0])
-// console.log(it[1])
-// console.log(it[3])
-it[0].style.backgroundColor = 'red';
+// let it = document.getElementsByTagName("li")
+// // console.log(it[0])
+// // console.log(it[1])
+// // console.log(it[3])
+// it[0].style.backgroundColor = 'red';
 
 
 // for(let i = 0;i<=it.length;i++){
 //     it[i].style.backgroundColor = 'red';
 // }
+
+
+//           QUERYSELECTOR
+
+// var header = document.querySelector(".list-group-item:nth-child(2)")
+// console.log(header)
+// console.log(header.style.backgroundColor='green');
+
+// var child = document.querySelector(".list-group-item:nth-child(3)")
+
+// console.log(child.style.display='None');
+
+
+// QUERYSELECTORALL
+
+// var header = document.querySelectorAll(".list-group-item")
+// console.log(header)
+// header[0].style.backgroundColor = "green"
+
+var odd = document.querySelectorAll(".list-group-item:nth-child(odd)")
+// console.log(odd)
+
+for(let i = 0;i<=odd.length;i++){
+        odd[i].style.backgroundColor = 'green';
+    }
+
+
+
