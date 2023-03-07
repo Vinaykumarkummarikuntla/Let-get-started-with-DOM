535b2d563b2940f6bd1836f17d81fdd749078cf8
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-03-07 14:44:52 +0530

    ElementsByClassName

--- Lets get started with DOM/main.js
+++ Lets get started with DOM/main.js
@@ -1,8 +1,17 @@
 var headerTitle=document.getElementById("header-title")
 var header = document.getElementById('main-header');
 console.log(header.style.borderBottom = 'solid 3px #000');
-console.log("hii")
+
 var item = document.getElementById("item")
 
 item.style.fontWeight = 'bold';
 item.style.color = 'green';
+
+//Using Class Name
+let items = document.getElementsByClassName("list-group-item")
+console.log(items)
+items[2].style.backgroundColor = 'yellow'
+for(let i = 0;i<=items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
+// item[2].style.backgroundColor = 'green'
