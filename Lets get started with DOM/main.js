369a179ccac2c59b886d552ef9390fda2d0bf282
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-03-07 12:34:22 +0530

    commit

---
+++ Lets get started with DOM/main.js
@@ -0,0 +1,8 @@
+var headerTitle=document.getElementById("header-title")
+var header = document.getElementById('main-header');
+console.log(header.style.borderBottom = 'solid 3px #000');
+console.log("hii")
+var item = document.getElementById("item")
+
+item.style.fontWeight = 'bold';
+item.style.color = 'green';
---
+++ window object/main.js
@@ -0,0 +1,5 @@
+list1 = document.getElementById('myList').getElementsByTagName('li')[0]
+list2 = document.getElementById('myList').getElementsByTagName('li')[1]
+
+list1.style.color = 'green';
+list2.style.color = 'yellow';
