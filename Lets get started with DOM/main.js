55a4801667cfcdf0bc7bc7f22749f537d80e8b91
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-03-07 15:06:56 +0530

    commit

--- Lets get started with DOM/main.js
+++ Lets get started with DOM/main.js
@@ -7,11 +7,24 @@ var item = document.getElementById("item")
 item.style.fontWeight = 'bold';
 item.style.color = 'green';
 
-//Using Class Name
-let items = document.getElementsByClassName("list-group-item")
-console.log(items)
-items[2].style.backgroundColor = 'yellow'
-for(let i = 0;i<=items.length;i++){
-    items[i].style.fontWeight = 'bold';
-}
-// item[2].style.backgroundColor = 'green'
+//     Using getElementsByClassName
+
+// let items = document.getElementsByClassName("list-group-item")
+// console.log(items)
+// items[2].style.backgroundColor = 'yellow'
+// for(let i = 0;i<=items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+//      Using getElementTagName
+
+let it = document.getElementsByTagName("li")
+// console.log(it[0])
+// console.log(it[1])
+// console.log(it[3])
+it[0].style.backgroundColor = 'red';
+
+
+// for(let i = 0;i<=it.length;i++){
+//     it[i].style.backgroundColor = 'red';
+// }
