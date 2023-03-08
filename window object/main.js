08eb637ceb000f55cd83f1587749482598a0c2da
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-03-08 09:19:30 +0530

    commit

--- window object/main.js
+++
@@ -1,5 +0,0 @@
-list1 = document.getElementById('myList').getElementsByTagName('li')[0]
-list2 = document.getElementById('myList').getElementsByTagName('li')[1]
-
-list1.style.color = 'green';
-list2.style.color = 'yellow';
